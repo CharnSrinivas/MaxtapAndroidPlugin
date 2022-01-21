370615c55eed2e0fe9068df3225cb11739f37837
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-01-21 15:47:29 +0530

    Unstable changes

--- app/src/main/java/com/example/maxtap_sdk/MainActivity.java
+++ app/src/main/java/com/example/maxtap_sdk/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.maxtap_sdk;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.maxtap.MaxTap;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
@@ -10,12 +12,19 @@ import com.google.android.exoplayer2.ui.PlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        super.onCreate(savedInstanceState);
+
+
         ExoPlayer exoPlayer = new ExoPlayer.Builder(this).build();
-        PlayerView playerView = findViewById(R.id.exoPlayerView);
+        PlayerView playerView = findViewById(R.id.video_view);
         //Binding the player with the view that is there in our xml.
         playerView.setPlayer(exoPlayer);
         //Building the media Item.
@@ -27,6 +36,13 @@ public class MainActivity extends AppCompatActivity {
         //playing the video.
         exoPlayer.play();
         String movieId = "test_data";
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("test_data","Just seen");
+        mFirebaseAnalytics.logEvent("android_test_event", bundle);
         new MaxTap(this, this, playerView, exoPlayer, movieId).init();
+
     }
+
 }
