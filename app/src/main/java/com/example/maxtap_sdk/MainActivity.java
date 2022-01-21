1f9555206f9a2583405133d6ad2a37dd68bc9b62
CHARAN SRINIVAS <charansrinivassajja@gmail.com>
2022-01-21 15:58:59 +0530

    Initial Commit

--- app/src/main/java/com/example/maxtap_sdk/MainActivity.java
+++ app/src/main/java/com/example/maxtap_sdk/MainActivity.java
@@ -4,44 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.brightcove.player.model.DeliveryType;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+import com.brightcove.player.view.BrightcovePlayer;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.maxtap.MaxTap;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.ui.PlayerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends BrightcovePlayer {
 
 
     private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        setContentView(R.layout.activity_main);
-
         super.onCreate(savedInstanceState);
-
-
-        ExoPlayer exoPlayer = new ExoPlayer.Builder(this).build();
-        PlayerView playerView = findViewById(R.id.video_view);
-        //Binding the player with the view that is there in our xml.
-        playerView.setPlayer(exoPlayer);
-        //Building the media Item.
-        MediaItem mediaItem = MediaItem.fromUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4");
-        //Setting the media item that is to be played.
-        exoPlayer.setMediaItem(mediaItem);
-        //preparing the player
-        exoPlayer.prepare();
-        //playing the video.
-        exoPlayer.play();
-        String movieId = "test_data";
-        // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        Bundle bundle = new Bundle();
-        bundle.putString("test_data","Just seen");
-        mFirebaseAnalytics.logEvent("android_test_event", bundle);
-        new MaxTap(this, this, playerView, exoPlayer, movieId).init();
+        setContentView(R.layout.activity_main);
+        brightcoveVideoView =  findViewById(R.id.video_player);
+        Video video = Video.createVideo("http://sdks.support.brightcove.com/assets/videos/hls/greatblueheron/greatblueheron.m3u8",
+                DeliveryType.HLS);
+        try {
+            java.net.URI myposterImage = new URI("https://sdks.support.brightcove.com/assets/images/general/Great-Blue-Heron.png");
+            video.getProperties().put(Video.Fields.STILL_IMAGE_URI, myposterImage);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        brightcoveVideoView.add(video);
+        brightcoveVideoView.start();
+//
+//        ExoPlayer exoPlayer = new ExoPlayer.Builder(this).build();
+//        PlayerView playerView = findViewById(R.id.video_view);
+//        //Binding the player with the view that is there in our xml.
+//        playerView.setPlayer(exoPlayer);
+//        //Building the media Item.
+//        MediaItem mediaItem = MediaItem.fromUri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4");
+//        //Setting the media item that is to be played.
+//        exoPlayer.setMediaItem(mediaItem);
+//        //preparing the player
+//        exoPlayer.prepare();
+//        //playing the video.
+//        exoPlayer.play();
+//        String movieId = "test_data";
+//        // Obtain the FirebaseAnalytics instance.
+//        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+//        Bundle bundle = new Bundle();
+//        bundle.putString("test_data","Just seen");
+//        mFirebaseAnalytics.logEvent("android_test_event", bundle);
+//        new MaxTap(this, this, playerView, exoPlayer, movieId).init();
 
     }
 
