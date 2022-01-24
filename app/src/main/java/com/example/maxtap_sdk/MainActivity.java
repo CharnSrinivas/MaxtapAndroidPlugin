3856219e22b46c2a7a255cdeac8f7b03e12f5816
Hitesh Jain <jainhitesh999@gmail.com>
2022-01-24 20:01:14 +0100

    Add analytics to VideoPlayer

--- app/src/main/java/com/example/maxtap_sdk/MainActivity.java
+++ app/src/main/java/com/example/maxtap_sdk/MainActivity.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAnalytics mFirebaseAnalytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +16,5 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.start_btn).setOnClickListener((start_button)->{
             startActivity(new Intent(MainActivity.this,VideoPlayer.class));
         });
-//         Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        mFirebaseAnalytics.setUserId("123456");
-        Bundle bundle = new Bundle();
-        bundle.putString("test_data","hello i am from Amsterdam");
-        mFirebaseAnalytics.logEvent("hitesh_test_event", bundle);
-
     }
 }
